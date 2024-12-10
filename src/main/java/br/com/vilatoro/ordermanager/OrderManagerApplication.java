e587e09938f19e994ec99f11739ae63372c90cd6
Guilherme Vilatoro <vilatorog@gmail.com>
2024-12-10 00:39:32 -0300

    Configuração do application.yml

--- src/main/java/br/com/vilatoro/ordermanager/OrderManagerApplication.java
+++ src/main/java/br/com/vilatoro/ordermanager/OrderManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagerorderApplication {
+public class OrderManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManagerorderApplication.class, args);
+		SpringApplication.run(OrderManagerApplication.class, args);
 	}
 
 }

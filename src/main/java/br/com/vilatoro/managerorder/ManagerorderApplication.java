098813f50aa19fdd3b496bd1ec9253836032a278
Guilherme Vilatoro Santos <guilherme.vilatoro@elotech.com.br>
2024-12-09 23:37:42 -0300

    Criação do Projeto de Gerenciamento de Pedidos

---
+++ src/main/java/br/com/vilatoro/managerorder/ManagerorderApplication.java
@@ -0,0 +1,13 @@
+package br.com.vilatoro.managerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vilatoro/managerorder/ManagerorderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vilatoro.managerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

78b060ccb28a3ae093c67cbca5f5b8b1e2a45a5b
Guilherme Vilatoro <vilatorog@gmail.com>
2024-12-12 23:12:36 -0300

    feature:Criação dos liquibases para criação das tabelas no banco de dados postgres.

--- src/main/java/br/com/vilatoro/ordermanager/domain/OrderEntity.java
+++ src/main/java/br/com/vilatoro/ordermanager/domain/OrderEntity.java
@@ -10,14 +10,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
+@Table(name = "orderservice")
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/br/com/vilatoro/ordermanager/domain/ProductEntity.java
+++ src/main/java/br/com/vilatoro/ordermanager/domain/ProductEntity.java
@@ -4,12 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-
-import java.math.BigDecimal;
 
 @Entity
 @Data
+@Table(name = "product")
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
